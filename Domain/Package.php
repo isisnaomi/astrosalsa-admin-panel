18ba98eb762914edb62989eb3fd7b9ecee2750c9
David Hernández <davidhernandeze@gmail.com>
2016-10-11 11:05:45 -0500

    Package identada

--- Domain/Package.php
+++ Domain/Package.php
@@ -1,49 +1,51 @@
-<?php
-	class Package {
+	<?php
+		class Package {
 
-			/**
-   		* @var string
-    	*/
-			private $packageID;
+				/**
+	   		* @var string
+	    	*/
+				private $packageID;
 
-			/**
-   		* @var int
-    	*/
-			private $classesIncluded;
+				/**
+	   		* @var int
+	    	*/
+				private $classesIncluded;
 
-			/**
-   		* @var float
-    	*/
-			private $price;
+				/**
+	   		* @var float
+	    	*/
+				private $price;
 
-			/**
-   		* @param
-    	*/
-	   	public function __construct( $packageID, $classesIncluded, $price ) {
-	   		$this->packageID = $packageID;
-	   		$this->classesIncluded = $classesIncluded;
-	   		$this->price = $price;
-	   }
+				/**
+	   		* @param string packageID
+				* @param int classesIncluded
+				* @param float price
+	    	*/
+		   	public function __construct( $packageID, $classesIncluded, $price ) {
+		   		$this->packageID = $packageID;
+		   		$this->classesIncluded = $classesIncluded;
+		   		$this->price = $price;
+		   }
 
-		 	/**
-  		* @return float
-			*/
-	   	public function getPrice() {
-	       return $this->price;
-	   }
+			 	/**
+	  		* @return float
+				*/
+		   	public function getPrice() {
+		       return $this->price;
+		   }
 
-			/**
-			* @return int
- 			*/
-	   	public function getClassesIncluded() {
-	       return $this->classesIncluded;
-	   }
+				/**
+				* @return int
+	 			*/
+		   	public function getClassesIncluded() {
+		       return $this->classesIncluded;
+		   }
 
-	    /**
-   		* @return string
-      */
-			public function getPackageID() {
-	       return $this->packageID;
-	   }
+		    /**
+	   		* @return string
+	      */
+				public function getPackageID() {
+		       return $this->packageID;
+		   }
 
-	 }
+		 }
