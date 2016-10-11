b8caa9e7e8202b7d65a1a78cae3857fa6551afbe
David Hernández <davidhernandeze@gmail.com>
2016-10-11 10:52:57 -0500

    Package comentada

--- Domain/Package.php
+++ Domain/Package.php
@@ -2,24 +2,47 @@
 
 	class Package {
 
+		/**
+   		* @var string
+    	*/
 		private $packageID;
+
+		/**
+   		* @var int
+    	*/
 		private $classesIncluded;
+
+		/**
+   		* @var float
+    	*/
 		private $price;
 
+		/**
+   		* @param 
+    	*/
 	   public function __construct( $packageID, $classesIncluded, $price ) {
 	   		$this->packageID = $packageID;
 	   		$this->classesIncluded = $classesIncluded;
 	   		$this->price = $price;
 	   }
 
+	    /**
+   		* @return float
+       	*/
 	   public function getPrice() {
 	       return $this->price;
 	   }
 
+		/**
+   		* @return int
+       	*/
 	   public function getClassesIncluded() {
 	       return $this->classesIncluded;
 	   }
 
+	    /**
+   		* @return string
+       	*/
 	    public function getPackageID() {
 	       return $this->packageID;
 	   }
