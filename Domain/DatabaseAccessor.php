df7226209e95753269f103e9b6a4eb4aa6e86b8d
Erick Alejandro Montañez Sodá <codeams@MacBook-Air-de-Erick.local>
2016-11-21 17:59:10 -0600

    Renombrando clase Database

---
+++ Domain/DatabaseAccessor.php
@@ -0,0 +1,185 @@
+<?php
+
+require_once '../Domain/QueryGenerator.php';
+
+/**
+ * Database
+ * Black box Class
+ * Provides an interface for the access for a specified database.
+ * Uses PHP 5 standard functions.
+ */
+class DataBase {
+
+  /**
+   * @var
+   */
+  private $name;
+
+  /**
+   * @var
+   */
+  private $tableName;
+
+  /**
+   * @var
+   */
+  private $server;
+
+  /**
+   * @var
+   */
+  private $connection;
+
+
+  /**
+   * DataBase constructor.
+   *
+   * @param $dataBaseName
+   * @param $tableName
+   * @param string $serverName
+   */
+  public function __construct( $dataBaseName, $tableName, $serverName = 'localhost' ) {
+
+    $this->name = $dataBaseName;
+    $this->tableName = $tableName;
+    $this->server = $serverName;
+
+  }
+
+  /**
+  * @return string $this->name
+  */
+  public function getName() {
+
+    return $this->name;
+
+  }
+
+  /**
+  * @return string $lastDatabaseErrorMessage
+  */
+  public function getErrorMessage() {
+
+    $lastDatabaseErrorMessage = mysql_error( $this->connection );
+    return $lastDatabaseErrorMessage;
+
+  }
+
+  /**
+  * @param  string  $username
+  * @param  string  $password = null
+  * @return string  $isConnectionEstablished
+  */
+  public function connect( $username, $password = null ) {
+
+    $isPasswordDefined = ! is_null( $password );
+
+    if ( $isPasswordDefined ) {
+
+      $this->connection = mysql_connect( $this->server, $username, $password );
+
+    } else {
+
+      $this->connection = mysql_connect( $this->server, $username );
+
+    }
+
+    $isConnectionEstablished = mysql_select_db( $this->name, $this->connection );
+
+    return $isConnectionEstablished;
+
+  }
+
+  /**
+  * @return boolean $isDisconnected
+  */
+  public function disconnect() {
+
+    $isDisconnected = mysql_close( $this->connection );
+    return $isDisconnected;
+
+  }
+
+  /**
+   * @param  string[] $attributes
+   * @param  string $rowFilters
+   * @return array|bool
+   * ] | false
+   */
+  public function selectRows( $attributes, $rowFilters = null ) {
+
+  	$query = QueryGenerator::generateSelectRowsQuery( $this->tableName, $attributes, $rowFilters );
+
+    $areRowsFetched = mysql_query( $query, $this->connection );
+
+    if ( $areRowsFetched ) {
+
+      $fetchedRows = $areRowsFetched;
+      $selectedRows = array();
+
+      while ( $row = mysql_fetch_assoc( $fetchedRows ) ) {
+
+        array_push( $selectedRows, $row );
+
+      }
+
+      return $selectedRows;
+
+    } else {
+
+      return false;
+
+    }
+
+  }
+
+  /**
+   * @param  [ attribute => attributeValue ][]  $row
+   * @return bool $isRowInserted
+   */
+  public function insertRow( $row ) {
+
+    $query = QueryGenerator::generateInsertRowQuery( $this->tableName, $row );
+
+    $isRowInserted = mysql_query( $query, $this->connection );
+
+    if ( $isRowInserted ) return true;
+    else return false;
+
+  }
+
+  /**
+   * @param  string  $rowFilters
+   * @return boolean $isRowDeleted
+   */
+  public function deleteRow( $rowFilters ) {
+
+    $query = QueryGenerator::generateDeleteRowQuery( $this->tableName, $rowFilters );
+
+    $isRowDeleted = mysql_query( $query, $this->connection );
+
+    if ( $isRowDeleted ) return true;
+    else return false;
+
+  }
+
+  /**
+   * @param string[] $attributes
+   * @param string $rowFilters
+   * @return bool
+   */
+  public function updateRow( $attributes, $rowFilters ) {
+
+    $query = QueryGenerator::generateUpdateRowQuery( $this->tableName, $attributes, $rowFilters );
+
+    $isRowUpdated = mysql_query( $query );
+
+    if ( $isRowUpdated ) {
+      return true;
+    } else {
+      return false;
+    }
+
+  }
+
+}
