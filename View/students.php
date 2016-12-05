4ae0ea8253d3f1eb350954d5b58a83ffca6c253a
Erick Montañez <codeams@gmail.com>
2016-12-05 01:58:53 -0600

    Agregando funcionalidad a los quick access buttons

--- View/students.php
+++ View/students.php
@@ -33,6 +33,40 @@
         $('.main-menu')
           .find('li.students')
           .addClass('active');
+
+        function getQueryVariable( variable ) {
+
+          var query = window.location.search.substring( 1 );
+
+          var vars = query.split( '&' );
+
+          for ( var i = 0; i < vars.length; i++ ) {
+
+            var pair = vars[i].split( '=' );
+
+            if (pair[0] == variable)
+              return pair[1];
+
+          }
+
+          return false;
+        }
+
+        var action = getQueryVariable( 'action' );
+
+        if ( action ) {
+
+          if ( action === 'addStudent' ) {
+
+            $( 'iframe' ).attr( 'src', 'dashboard-students.php?action=addStudent' );
+
+          } else if ( action === 'payment' ) {
+
+            $( 'iframe' ).attr( 'src', 'dashboard-students.php?action=payment' );
+
+          }
+
+        }
       });
     </script>
 
